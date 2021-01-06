e822b91041a898c052652093a41d3e2cf40d086c
Robin Simard Lamarre <robinlamarre@gmail.com>
2021-01-06 15:36:16 -0500

    Select channel with configurable  time

--- select/racer.go
+++ select/racer.go
@@ -1,19 +1,35 @@
 package _select
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+var tenSecondTimeout = 10 * time.Second
 
-	if aDuration < bDuration {
-		return a
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("Time out waiting for %s and %s", a, b)
 	}
+}
 
-	return b
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
 
 func measureResponseTime(url string) time.Duration {
